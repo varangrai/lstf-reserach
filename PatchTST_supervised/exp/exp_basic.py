4005447774708539d7521d273e98f45c53861048
Samarth Singhal <54440645+a12dongithub@users.noreply.github.com>
2023-06-18 16:55:47 +0530

    fix again gpu

--- PatchTST_supervised/exp/exp_basic.py
+++ PatchTST_supervised/exp/exp_basic.py
@@ -16,8 +16,18 @@ class Exp_Basic(object):
 
     def _acquire_device(self):
         if self.args.use_gpu:
-            device = torch.device('mps')
-            print('Use GPU: mps')
+            if(torch.backends.mps.is_available()):
+                device = torch.device('mps')
+                print('Use GPU: mps')
+            elif self.args.use_gpu:
+                os.environ["CUDA_VISIBLE_DEVICES"] = str(
+                    self.args.gpu) if not self.args.use_multi_gpu else self.args.devices
+                device = torch.device('cuda:{}'.format(self.args.gpu))
+                print('Use GPU: cuda:{}'.format(self.args.gpu))
+            else:
+                device = torch.device('cpu')
+                print('Use CPU')
+
         else:
             device = torch.device('cpu')
             print('Use CPU')
