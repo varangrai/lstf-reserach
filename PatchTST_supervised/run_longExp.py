58fbda1d1e792763b1595dd2d5c0bd68426030ce
varangrai <varangrai@gmail.com>
2023-08-05 12:45:33 +0530

    undo

--- PatchTST_supervised/run_longExp.py
+++ PatchTST_supervised/run_longExp.py
@@ -90,7 +90,7 @@ parser.add_argument('--experiment_name', type=str, default='exp_unamed', help='e
 
 # GPU   
 parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
-parser.add_argument('--gpu', type=int, default=0,1, help='gpu')
+parser.add_argument('--gpu', type=int, default=0, help='gpu')
 parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
 parser.add_argument('--devices', type=str, default='0,1', help='device ids of multile gpus')
 parser.add_argument('--test_flop', action='store_true', default=False, help='See utils/tools for usage')
